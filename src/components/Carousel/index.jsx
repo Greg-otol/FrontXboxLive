0b5442eace543a4d5b4cc88e80c7f016625c909d
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2022-10-22 14:18:54 -0300

    Add carousel

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,48 @@
+import Carousel from 'react-bootstrap/Carousel';
+import elden from "../../assets/img/elden.png";
+import halo from "../../assets/img/halo.png";
+import forza from "../../assets/img/forza.png";
+
+export function HomeCarousel() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={halo}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={forza}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={elden}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
