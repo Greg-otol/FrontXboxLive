05c77f31f15b9f3ddec978f1cb51d43d6370ddfa
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2022-11-07 19:22:37 -0300

    updated url

--- src/Service/api.ts
+++ src/Service/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://localhost:3001/'
-    //baseURL: 'https://back-xbox-live.onrender.com'
+    //baseURL: 'http://localhost:3001/'
+    baseURL: 'https://back-xbox-live.onrender.com'
 })
 
 Api.interceptors.request.use((config: any) => {
